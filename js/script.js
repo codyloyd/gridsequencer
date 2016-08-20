5fb42d3c2889f57c43e84f6f42b28f050f59f8ce
Cody Loyd <codyloyd@gmail.com>
2016-08-20 15:36:12 -0500

    cleaned syntax

--- js/script.js
+++ js/script.js
@@ -1,33 +1,29 @@
-var seq2
+var seq
 $(document).ready(function () {
   grid[0][0] = 'x'
-  grid[0][4] = 'x'
-  grid[4][7] = 'x'
   renderGrid(grid)
-  seq2 = makeSeq(grid)
+  seq = makeSeq(grid)
   $('button').click(function () {
-    loopSeq(seq2)
+    loopSeq(seq)
   })
-
 })
 
 function blockListener () {
   $('.block').click(function () {
-    grid[$(this).attr("x")][$(this).attr("y")] = "x"
+    grid[$(this).attr('x')][$(this).attr('y')] = 'x'
     renderGrid(grid)
-    seq2 = makeSeq(grid)
-    console.log(seq2)
+    seq = makeSeq(grid)
   })
 }
 
 var grid = generateGrid(16)
 function generateGrid (x) {
   var grid = []
-  for (var i = 0; i < x; i++)   {
+  for (var i = 0; i < x; i++) {
     grid.push(function generateRow () {
       var array = []
       for (var ii = 0; ii < x; ii++) {
-        array.push(" ")
+        array.push(' ')
       }
       return array
     }())
@@ -40,7 +36,7 @@ function renderGrid (grid) {
   for (var i = 0; i < grid.length; i++) {
     $('.container').append('<div class="column "id="column' + i + '"></div>')
     for (var j = 0; j < grid[i].length; j++) {
-      $('#column' + i + '').append('<div class="block" x=' + i + ' y=' + j + '>' + grid[i][j] + '</div>')
+      $('#column' + i + '').append('<div class="block ' + grid[i][j] + '" x=' + i + ' y=' + j + '>' + grid[i][j] + '</div>')
     }
   }
   blockListener()
@@ -51,7 +47,7 @@ function makeSeq (grid) {
   for (var i = 0; i < grid.length; i++) {
     var step = []
     for (var j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] !== " ") {
+      if (grid[i][j] !== ' ') {
         step.push(j)
       }
     }
@@ -60,22 +56,20 @@ function makeSeq (grid) {
   return seq
 }
 
-var seq7 = [[0,14], [2], [], [4], [7], [12], [0,9], [2], [], [4], [7], [12]]
 var count = 0
-
 function seqCount (seq) {
   var note = seq[count % seq.length]
   count += 1
   return note
 }
 
-function loopSeq (seq) {
-  playStep(seqCount(seq2))
-  setTimeout(function () { loopSeq(seq2) }, 200)
+function loopSeq () {
+  playStep(seqCount(seq))
+  setTimeout(function () { loopSeq(seq) }, 200)
 }
 
 function playStep (step) {
-  if (step.length > 0 ) {
+  if (step.length > 0) {
     for (var i = 0; i < step.length; i++) {
       var p = mtof(step[i] + 72)
       saw.play({
