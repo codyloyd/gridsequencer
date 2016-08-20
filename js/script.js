3c390ce834a2cc6719f8e1e660d6dd171c0984ef
Cody Loyd <codyloyd@gmail.com>
2016-08-20 15:23:54 -0500

    grid works

--- js/script.js
+++ js/script.js
@@ -1,22 +1,88 @@
+var seq2
 $(document).ready(function () {
+  grid[0][0] = 'x'
+  grid[0][4] = 'x'
+  grid[4][7] = 'x'
+  renderGrid(grid)
+  seq2 = makeSeq(grid)
   $('button').click(function () {
-    loopSeq()
+    loopSeq(seq2)
   })
+
 })
 
-var seq = [0, 2, 9, 4, 7, 12]
+function blockListener () {
+  $('.block').click(function () {
+    grid[$(this).attr("x")][$(this).attr("y")] = "x"
+    renderGrid(grid)
+    seq2 = makeSeq(grid)
+    console.log(seq2)
+  })
+}
+
+var grid = generateGrid(16)
+function generateGrid (x) {
+  var grid = []
+  for (var i = 0; i < x; i++)   {
+    grid.push(function generateRow () {
+      var array = []
+      for (var ii = 0; ii < x; ii++) {
+        array.push(" ")
+      }
+      return array
+    }())
+  }
+  return grid
+}
+
+function renderGrid (grid) {
+  $('.container').empty()
+  for (var i = 0; i < grid.length; i++) {
+    $('.container').append('<div class="column "id="column' + i + '"></div>')
+    for (var j = 0; j < grid[i].length; j++) {
+      $('#column' + i + '').append('<div class="block" x=' + i + ' y=' + j + '>' + grid[i][j] + '</div>')
+    }
+  }
+  blockListener()
+}
+
+function makeSeq (grid) {
+  var seq = []
+  for (var i = 0; i < grid.length; i++) {
+    var step = []
+    for (var j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] !== " ") {
+        step.push(j)
+      }
+    }
+    seq.push(step)
+  }
+  return seq
+}
+
+var seq7 = [[0,14], [2], [], [4], [7], [12], [0,9], [2], [], [4], [7], [12]]
 var count = 0
+
 function seqCount (seq) {
   var note = seq[count % seq.length]
   count += 1
   return note
 }
 
-function loopSeq () {
-  saw.play({
-    pitch: mtof(seqCount(seq) + 72)
-  })
-  setTimeout(function () { startSeq() }, 300)
+function loopSeq (seq) {
+  playStep(seqCount(seq2))
+  setTimeout(function () { loopSeq(seq2) }, 200)
+}
+
+function playStep (step) {
+  if (step.length > 0 ) {
+    for (var i = 0; i < step.length; i++) {
+      var p = mtof(step[i] + 72)
+      saw.play({
+        pitch: p
+      })
+    }
+  }
 }
 
 var saw = new Wad({
